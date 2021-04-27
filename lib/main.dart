acd094fac0bc51f43e3742be1b60c70d16c87fb2
arytio <tioagung34@gmail.com>
2021-04-27 20:02:48 +0800

    Tugas Layoting

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Login Application',
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      home: new LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+enum FormType { login, register }
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailFilter = new TextEditingController();
+  final TextEditingController _passwordFilter = new TextEditingController();
+  String _email = "";
+  String _password = "";
+  FormType _form = FormType.login;
+
+  _LoginPageState() {
+    _emailFilter.addListener(_emailListen);
+    _passwordFilter.addListener(_passwordListen);
+  }
+
+  void _emailListen() {
+    if (_emailFilter.text.isEmpty) {
+      _email = "";
+    } else {
+      _email = _emailFilter.text;
+    }
+  }
+
+  void _passwordListen() {
+    if (_passwordFilter.text.isEmpty) {
+      _password = "";
+    } else {
+      _password = _passwordFilter.text;
+    }
+  }
+
+  void _formChange() async {
+    setState(() {
+      if (_form == FormType.register) {
+        _form = FormType.login;
+      } else {
+        _form = FormType.register;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: _buildBar(context),
+      body: new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Column(
+          children: <Widget>[
+            _buildTextFields(),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      title: new Text("Login aplikasi Manga Store"),
+      centerTitle: true,
+    );
+  }
+
+  Widget _buildTextFields() {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Container(
+            child: new TextField(
+              controller: _emailFilter,
+              decoration: new InputDecoration(labelText: 'Username/Gmail'),
+            ),
+          ),
+          new Container(
+            child: new TextField(
+              controller: _passwordFilter,
+              decoration: new InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    if (_form == FormType.login) {
+      return new Container(
+        child: new Column(
+          children: <Widget>[
+            new ElevatedButton(
+              child: new Text('Login'),
+              onPressed: _loginPressed,
+            ),
+            new TextButton(
+              child: new Text('Tidak Punya Akun ? Tap disini Untuk register.'),
+              onPressed: _formChange,
+            ),
+            new TextButton(
+              child: new Text('Lupa Password?'),
+              onPressed: _passwordReset,
+            )
+          ],
+        ),
+      );
+    } else {
+      return new Container(
+        child: new Column(
+          children: <Widget>[
+            new ElevatedButton(
+              child: new Text('Bikin Akun'),
+              onPressed: _createAccountPressed,
+            ),
+            new TextButton(
+              child: new Text('Punya Akun ? Click Disini Untuk  sign in.'),
+              onPressed: _formChange,
+            )
+          ],
+        ),
+      );
+    }
+  }
+
+  void _loginPressed() {
+    print('The user wants to login with $_email and $_password');
+  }
+
+  void _createAccountPressed() {
+    print('The user wants to create an accoutn with $_email and $_password');
+  }
+
+  void _passwordReset() {
+    print("The user wants a password reset request sent to $_email");
+  }
+}
